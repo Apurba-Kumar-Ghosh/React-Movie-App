65c550d551eb522afddfe8cfb568ab8c78078f47
Apurba Kr. Ghosh <apurba.kumarghosh.ece22@heritageit.edu.in>
2020-09-08 21:24:59 +0530

    Updates

---
+++ precache-manifest.ef7456ebadc6aa74bee434f196eeda2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d8f96355a54dcaee4b2c1dd3ab7eed",
+    "url": "/React-Movie-App/index.html"
+  },
+  {
+    "revision": "efd0d0d3e1325cc22b1a",
+    "url": "/React-Movie-App/static/css/main.cb727571.chunk.css"
+  },
+  {
+    "revision": "e596c22816c72c08db7c",
+    "url": "/React-Movie-App/static/js/2.4f11fa86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-App/static/js/2.4f11fa86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd0d0d3e1325cc22b1a",
+    "url": "/React-Movie-App/static/js/main.76f7f3ff.chunk.js"
+  },
+  {
+    "revision": "a08c065b026dcdd08ca2",
+    "url": "/React-Movie-App/static/js/runtime-main.452706db.js"
+  }
+]);
